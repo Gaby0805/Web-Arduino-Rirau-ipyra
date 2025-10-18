60ad2d7477c47674fe245a80058935150e2303da
Gaby <gabbiebrabo@gmail.com>
2025-10-17 23:12:50 -0400

    alter url axios

--- app/src/api.ts
+++ app/src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_ROUTE,
+  baseURL: "https://arduino-rirau-ipyra.onrender.com",
   withCredentials: true,
 });
 
