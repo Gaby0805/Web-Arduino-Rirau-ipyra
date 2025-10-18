eb493bbfa1dea7214580c6f7b9906475f002cdfe
Gaby <gabbiebrabo@gmail.com>
2025-10-18 17:50:47 -0400

    middle

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizeCss: false, // Desativa lightningcss
   },
 };
 
